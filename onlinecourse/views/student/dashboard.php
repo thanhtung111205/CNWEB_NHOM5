ec317ac505db46fb899affbe148f1a1b296e2d90
NguyenYen317 <nguyenthiyen31072004@gmail.com>
2025-12-04 23:01:39 +0700

    First add views/student/dashboard.php

---
+++ onlinecourse/views/student/dashboard.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>views/student/dashboard.php</title>
+</head>
+<body>
+    <h2>Bảng điều khiển sinh viên</h2>
+    <a href="/student/my_courses">Khóa học của tôi</a>
+</body>
+</html>
